e3e01d7ab18aad6782d326ce4573b0b429f95dd9
KEERTHA PRABHU S <keerthaprabhu32@gmail.com>
2026-06-22 11:49:47 +0530

    Move secret key to environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = 'django-insecure-change-later'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
